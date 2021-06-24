5f7d6b643d192d4ce6475aa7dbb9cf0fd16fc11a
can-dy-jack <jjack_chen3@163.com>
2021-06-24 16:42:06 +0800

    gpa计算函数封装一下

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,30 @@
+function score2gpa(score){
+    if(score<60){
+        data = 0.0
+    }else if(score <64){
+        data = 1.0
+    }else if(score < 66){
+        data = 1.5
+    }else if(score < 68){
+        data = 1.7
+    }else if(score < 72){
+        data = 2.0
+    }else if(score < 75){
+        data = 2.3
+    }else if(score < 78){
+        data = 2.7
+    }else if(score < 82){
+        data = 3.0
+    }else if(score < 85){
+        data = 3.3
+    }else if(score < 90){
+        data = 3.7
+    }else{
+        data = 4.0
+    }
+    return data
+}
+
 const cal = document.getElementById('cal');
 const input = document.getElementsByTagName('input');
 
@@ -7,35 +34,10 @@ cal.addEventListener('click',()=>{
     // let len = input.length
     // for(i=0;i<len;i++)
     if(res.textContent == "你的绩点是：" & input[0].value != null){
-        
-        if(input[0].value<60){
-            data = 0.0
-        }else if(input[0].value <64){
-            data = 1.0
-        }else if(input[0].value <66){
-            data = 1.5
-        }else if(input[0].value <68){
-            data = 1.7
-        }else if(input[0].value <72){
-            data = 2.0
-        }else if(input[0].value <75){
-            data = 2.3
-        }else if(input[0].value <78){
-            data = 2.7
-        }else if(input[0].value <82){
-            data = 3.0
-        }else if(input[0].value <85){
-            data = 3.3
-        }else if(input[0].value <90){
-            data = 3.7
-        }else{
-            data = 4.0
-        }
-        
+        data = score2gpa(input[0].value)
         res.textContent += data;
         // input[0].value
         // res.innerHTML += input[1].value; 
-
     }
 })
 
