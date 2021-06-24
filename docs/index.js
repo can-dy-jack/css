184b744c87944d969e0062d961ba5d244376acd9
can-dy-jack <jjack_chen3@163.com>
2021-06-24 16:34:13 +0800

    - add more columns - gpa

--- docs/index.js
+++ docs/index.js
@@ -7,9 +7,36 @@ cal.addEventListener('click',()=>{
     // let len = input.length
     // for(i=0;i<len;i++)
     if(res.textContent == "你的绩点是：" & input[0].value != null){
-        res.textContent += input[0].value
+        
+        if(input[0].value<60){
+            data = 0.0
+        }else if(input[0].value <64){
+            data = 1.0
+        }else if(input[0].value <66){
+            data = 1.5
+        }else if(input[0].value <68){
+            data = 1.7
+        }else if(input[0].value <72){
+            data = 2.0
+        }else if(input[0].value <75){
+            data = 2.3
+        }else if(input[0].value <78){
+            data = 2.7
+        }else if(input[0].value <82){
+            data = 3.0
+        }else if(input[0].value <85){
+            data = 3.3
+        }else if(input[0].value <90){
+            data = 3.7
+        }else{
+            data = 4.0
+        }
+        
+        res.textContent += data;
+        // input[0].value
+        // res.innerHTML += input[1].value; 
+
     }
-    // res.innerHTML += input[1].value; 
 })
 
 const clc = document.getElementById('clean');
