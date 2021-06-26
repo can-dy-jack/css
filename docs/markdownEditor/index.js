c6eb85f3ca5d74e3641e4e91b27a75a8280fcdc7
can-dy-jack <jjack_chen3@163.com>
2021-06-26 20:21:07 +0800

    tasklists

--- docs/markdownEditor/index.js
+++ docs/markdownEditor/index.js
@@ -2,7 +2,9 @@ const mdTxtArea = document.getElementById('md-txt-area');
 const md = document.getElementById("md");
 
 mdTxtArea.addEventListener('keyup',()=>{
-    var converter = new showdown.Converter();
+    var converter = new showdown.Converter({
+            tables: true,optionKey: 'value',tasklists: true
+    });
     var html = converter.makeHtml(mdTxtArea.value);
     md.innerHTML = html;
 })
