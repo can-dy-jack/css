dca65f2cd99df04cca788ab1b283642ce25f723d
can-dy-jack <jjack_chen3@163.com>
2021-06-27 10:56:29 +0800

    fix

--- markdownEditor/index.js
+++
@@ -1,8 +0,0 @@
-const mdTxtArea = document.getElementById('md-txt-area');
-const md = document.getElementById("md");
-
-mdTxtArea.addEventListener('keyup',()=>{
-    var converter = new showdown.Converter();
-    var html = converter.makeHtml(mdTxtArea.value);
-    md.innerHTML = html;
-})
